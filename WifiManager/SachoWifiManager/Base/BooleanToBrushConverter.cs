03751989d7cb1a41df98cb8cc4552a5501662bad
Shaco <zhangranmm@126.com>
2019-05-20 14:53:28 +0800

    调整样式

---
+++ WifiManager/SachoWifiManager/Base/BooleanToBrushConverter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace SachoWifiManager.Base
+{
+    /// <summary>
+    /// This converts a Boolean value to a Brush
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(Brush))]
+    public class BooleanToBrushConverter : MarkupExtension, IValueConverter
+    {
+        /// <summary>
+        /// Mapping for True value
+        /// </summary>
+        public Brush TrueBrush { get; set; }
+        /// <summary>
+        /// Mapping for False value
+        /// </summary>
+        public Brush FalseBrush { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public BooleanToBrushConverter()
+        {
+            TrueBrush = new SolidColorBrush(Colors.Blue);
+            FalseBrush = new SolidColorBrush(Colors.Black);
+        }
+
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value produced by the binding source.</param><param name="targetType">The type of the binding target property.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(Brush))
+                throw new ArgumentException("BoolToBrushConverter used inappropriately.");
+
+            bool flag = false;
+            if (value != null && value is bool)
+            {
+                flag = (bool)value;
+            }
+
+            return flag ? TrueBrush : FalseBrush;
+        }
+
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value that is produced by the binding target.</param><param name="targetType">The type to convert to.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns the converter
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new BooleanToBrushConverter { TrueBrush = TrueBrush, FalseBrush = FalseBrush };
+        }
+    }
+}
